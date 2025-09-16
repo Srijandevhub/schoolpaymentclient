d0fc47b42212f131371c5a452dcf1664fb4cff95
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-09-16 16:30:29 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Sidebar.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeSidebar } from '../utils/tooglerSlice';
+import axios from 'axios';
+import { baseUrl } from '../baseUrl';
+import { logout } from '../utils/userSlice';
+import { Form } from 'react-bootstrap';
+
 const Sidebar = ({ menuActive = "dashboard" }) => {
     const toogler = useSelector((state) => state.toogler.show);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        dispatch(logout());
+        navigate('/login');
+    }
     return (
         <div className={`card h-100 ${styles.sidebar} ${toogler ? `${styles.show}` : ""}`}>
             <div className={`card-header ${styles.sidebarHeader}`}>
                 <div className='d-flex justify-content-end'>
-                    <button className={styles.closeBtn} onClick={() => dispatch(closeSidebar())}>
+                    <button className={styles.closeBtn} onClick={() => dispatch(closeSidebar())} aria-label='close'>
                         <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                        <path fillEule="evenodd" d="M10 4H4c-1.10457 0-2 .89543-2 2v12c0 1.1046.89543 2 2 2h6V4ZM7.79283 9.29289c.39053.39053.39053 1.02371 0 1.41421L6.5 11.9999l1.29283 1.2929c.39053.3905.39053 1.0237 0 1.4142-.39052.3905-1.02368.3905-1.41421 0l-1.99994-2c-.39052-.3905-.39052-1.0236 0-1.4142l1.99994-1.99991c.39053-.39052 1.02369-.39052 1.41421 0Z" clipRule="evenodd"/>
+                        <path fillRule="evenodd" d="M10 4H4c-1.10457 0-2 .89543-2 2v12c0 1.1046.89543 2 2 2h6V4ZM7.79283 9.29289c.39053.39053.39053 1.02371 0 1.41421L6.5 11.9999l1.29283 1.2929c.39053.3905.39053 1.0237 0 1.4142-.39052.3905-1.02368.3905-1.41421 0l-1.99994-2c-.39052-.3905-.39052-1.0236 0-1.4142l1.99994-1.99991c.39053-.39052 1.02369-.39052 1.41421 0Z" clipRule="evenodd"/>
                         <path d="M12 20h8c1.1046 0 2-.8954 2-2V6c0-1.10457-.8954-2-2-2h-8v16Z"/>
                         </svg>
                     </button>
-                </div> 
+                </div>
             </div>
             <div className='card-body'>
                 <ul className={styles.sidebarList}>
@@ -44,6 +54,9 @@ const Sidebar = ({ menuActive = "dashboard" }) => {
                         </Link>
                     </li>
                 </ul>
+            </div>
+            <div className='card-footer'>
+                <button className='btn btn-danger w-100 btn-sm' onClick={handleLogout}>Logout</button>
             </div>
         </div>
     )
