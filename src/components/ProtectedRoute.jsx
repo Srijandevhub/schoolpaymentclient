a5322015afbc1fe8a62d0e143c1ed49c8c21c25a
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-09-16 16:30:13 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Loader from "./Loader";
 
 const ProtectedRoute = ({ children, isAuthpage = false }) => {
     const user = useSelector((state) => state.user);
     if (user.status === 'loading' || user.status === 'idle') {
-        return <h1>Loading...</h1>
+        return <Loader height="full"/>
     }
     if (isAuthpage) {
         if (user.data && user.isAuthenticated) {
