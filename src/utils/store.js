bb732de86cc36cd1877a72067456650b2cfe8f4a
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-09-16 16:31:17 +0530

    Update store.js

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './userSlice'
 import tooglerReducer from './tooglerSlice'
+import themeReducer from './themeSlice'
 const store = configureStore({
     reducer: {
         user: userReducer,
-        toogler: tooglerReducer
+        toogler: tooglerReducer,
+        theme: themeReducer
     }
 })
 export default store;
